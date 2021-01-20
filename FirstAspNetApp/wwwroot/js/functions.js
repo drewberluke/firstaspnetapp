caca5f6298c084f6de6e66589751105c56619d51
Andrew Luke <aluke4@byu.edu>
2021-01-19 17:04:38 -0700

    Initial check-in of module FirstAspNetApp

---
+++ FirstAspNetApp/wwwroot/js/functions.js
@@ -0,0 +1,14 @@
+﻿var submit = document.getElementById('submit_btn');
+
+submit.addEventListener('click', function () {
+    from = document.getElementById('from').value;
+    email = document.getElementById('email').value;
+    subject = document.getElementById('subject').value;
+    message = document.getElementById('message').value;
+    output =
+        "From: " + from + ',\n' +
+        "Email: " + email + ',\n' +
+        "Subject: " + subject + ',\n' +
+        "Message: " + message + ".";
+    alert(output);
+});
